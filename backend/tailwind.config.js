ba57e5c828df5bbffbd8a4e8132a547c4c97ca24
Bia Saldanha <abmsaldanha@gmail.com>
2024-12-16 17:22:46 +0000

    up1

--- backend/tailwind.config.js
+++ backend/tailwind.config.js
@@ -1,9 +1,11 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+      "./*.html", // Ficheiros HTML na raiz do projeto
+      "./frontend/**/*.html", // Todos os ficheiros HTML na pasta frontend
+      "./frontend/assets/js/**/*.js", // Todos os ficheiros JS na pasta assets/js
+  ],
   theme: {
-    extend: {},
+      extend: {},
   },
   plugins: [],
-}
-
+};
